0f49fdfd1907157c40795b4b1bd236039011267b
sithsiri <sith@live.com>
2018-08-07 13:45:33 -0500

    more
    now it gets the url

--- dankchat.py
+++ dankchat.py
@@ -1,3 +1,6 @@
+#should only work on Python 3.x
 import json
 import urllib.request
 endpointJSON = urllib.request.urlopen("http://chat.danktronics.org/api/v1/gateway/endpoint").read()
+endpointURL = json.load(endpointJSON)
+print endpointURL["url"]
