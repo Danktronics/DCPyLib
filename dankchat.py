24c326f49445bdfb165bc263cce47fbe9e9e11f4
sithsiri <sith@live.com>
2018-08-08 11:23:05 -0500

    slightly working
    xD

--- dankchat.py
+++ dankchat.py
@@ -1,6 +1,16 @@
-#should only work on Python 3.x
+# should only work on Python 3.x
 import json
-import urllib.request
-endpointJSON = urllib.request.urlopen("http://chat.danktronics.org/api/v1/gateway/endpoint").read()
-endpointURL = json.load(endpointJSON)
-print endpointURL["url"]
+import requests
+import asyncio
+import ssl
+import websockets
+async def connect(token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEQU5LVFJPTklDU0NIQVQiLCJpZCI6MTR9.CqAQt5-2MZdeDAvFVi-imZDCMiqDRSyp2i0V58CB6No"):
+	endpointJSON = requests.get("https://chat.danktronics.org/api/v1/gateway/endpoint")
+	endpointURL = json.loads(endpointJSON.content.decode("utf-8"))["url"] + "?token=" + token
+	print(endpointURL)
+	async with websockets.connect(endpointURL, ssl = True) as chatwebsocket:
+		pass
+
+asyncio.get_event_loop().run_forever(connect())
+
+# input("")
